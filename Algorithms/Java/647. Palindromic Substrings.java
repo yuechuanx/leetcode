7e541c3a50b82cfa3f2d885be20c830be2f1f191
Yue-chuan Xiao <xiaoyuechuanz@163.com>
2019-01-15 17:47:14 +0800

    update p647 in java

---
+++ Algorithms/Java/647. Palindromic Substrings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countSubstrings(String s) {
+        if (s.length() <= 1) return s.length();
+        
+        int cnt = s.length();
+        for (int step = 1; step < s.length(); step++) {
+            for (int i = 0; i < s.length() - step; ++i) {
+                int left = i, right = left + step;
+                if (isPalindromic(s, left, right)) 
+                    cnt++;
+            } 
+        }
+        
+        return cnt;
+    }
+    
+    boolean isPalindromic(String s, int left, int right) {
+        char[] str = s.toCharArray();
+        
+        while (left < right) {
+            if (str[left] != str[right]) return false;
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
