f4500e6221319304640be923366d4f2b70e9f6df
Yuechuan <xiaoyuechuanz@163.com>
2019-01-17 20:23:15 +0800

    modify function name

--- Algorithms/Java/39. Combination Sum.java
+++ Algorithms/Java/39. Combination Sum.java
@@ -1,19 +1,19 @@
-// backTrace. Time
+// backtrack. Time
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> res = new ArrayList<>();
-        backTrace(res, new ArrayList<Integer>(), candidates, target, 0);
+        backtrack(res, new ArrayList<Integer>(), candidates, target, 0);
         return res;
     }
     
-    private void backTrace(List<List<Integer>> res, List<Integer> path, int[] candidates, int target, int level) {
+    private void backtrack(List<List<Integer>> res, List<Integer> path, int[] candidates, int target, int level) {
         if (target == 0) {
             res.add(new ArrayList<Integer>(path));
         }
         for (int i = level; i < candidates.length && target - candidates[i] >= 0; ++i) {
                 path.add(candidates[i]);
-                backTrace(res, path, candidates, target - candidates[i], i);
+                backtrack(res, path, candidates, target - candidates[i], i);
                 path.remove(path.size() - 1);
         }
     }
--- Algorithms/Java/77. Combinations.java
+++ Algorithms/Java/77. Combinations.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res = new ArrayList<>();
-        backTrace(res, new ArrayList<Integer>(), n, k, 1);
+        backtrack(res, new ArrayList<Integer>(), n, k, 1);
         return res;
     }
     
-    private void backTrace(List<List<Integer>> res, List<Integer> path, int n, int k, int start) {
+    private void backtrack(List<List<Integer>> res, List<Integer> path, int n, int k, int start) {
         if (k == 0) {
             res.add(new ArrayList<Integer>(path));
             return;
         }
         for (int i = start; i <= n; ++i) {
             path.add(i);
-            backTrace(res, path, n, k - 1, i + 1);
+            backtrack(res, path, n, k - 1, i + 1);
             path.remove(path.size() - 1);
         }
     }
