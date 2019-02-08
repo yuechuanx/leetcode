90963b006b313eeb4b25115666ae04c0f0fc0955
Yuechuan <xiaoyuechuanz@163.com>
2019-02-08 13:08:12 +0800

    update p105 p897

---
+++ Algorithms/Java/897. Increasing Order Search Tree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode curr;
+    
+    public TreeNode increasingBST(TreeNode root) {
+        TreeNode res = new TreeNode(-1);
+        curr = res;
+        inorder(root);
+        return res.right;
+    }
+    
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+        inorder(node.left);
+        node.left = null;
+        curr.right = node;
+        curr = node;
+        inorder(node.right);
+    }
+}
