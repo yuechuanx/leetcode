c735e03e1810057a543f36142c6a13c23fe82393
Yuechuan Xiao <xiaoyuechuanz@163.com>
2019-03-03 10:24:08 +0800

    update p121, p5, p745

--- Algorithms/Java/P121-Best-Time-to-Buy-and-Sell-Stock.java
+++ Algorithms/Java/P121-Best-Time-to-Buy-and-Sell-Stock.java
@@ -1,3 +1,21 @@
+/* best solution */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        
+        for (int price : prices) {
+            if (price < minprice) {
+                minprice = price;
+            } else {
+                maxprofit = Math.max(maxprofit, price - minprice);
+            }
+        }
+        
+        return maxprofit;
+    }
+}
+
 class Solution {
     public int maxProfit(int[] prices) {
         if(prices.length == 0 || prices.length == 1) return 0;
@@ -21,3 +39,47 @@ class Solution {
         return res;
     }
 }
+
+/**
+ * brute-force
+ * 这代码写得真烂
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0 || prices.length == 1) {
+            return 0;
+        }
+        
+        int[] profit = new int[prices.length];
+        
+        for (int i = 0; i < prices.length; ++i) {
+            for (int j = i + 1; j < prices.length; ++j) {
+                    profit[i] = Math.max(profit[i], prices[j] - prices[i]);
+            }
+        }
+
+        int res = -1;
+        for (int p : profit) {
+            res = Math.max(res, p);
+        }
+        
+        return res;
+    }
+}
+
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int maxprofit = 0;
+
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit)
+                    maxprofit = profit;
+            }
+        }
+        
+        return maxprofit;
+    }
+}
+
--- Algorithms/Java/P5-Longest-Palindromic-Substring.java
+++ Algorithms/Java/P5-Longest-Palindromic-Substring.java
@@ -5,6 +5,8 @@ class Solution {
         if (s.length() < 2 || s == null) return s;
         
         char[] str = s.toCharArray();
+
+        /* 逐个位置检查回文 */
         for (int i = 0; i < s.length(); ++i) {
             helper(str, i, i);
             helper(str, i, i + 1);
@@ -19,45 +21,10 @@ class Solution {
             right++;
         }
 
-        // sample: "aba", right -left = 2,  rightLen = right -left + 1
-        // after while loop: left--, right++, rightLen -= 2;
-        int currLen = right -left - 1;
+        int currLen = right -left - 1;  // 出循环时, 多执行了一次 while
         if (currLen > palinLen) {
             palinLen = currLen;
-            palinStart = left + 1;  // remember after while loop left -= 1;
+            palinStart = left + 1;
         }
-    }
-}
-
-public class Solution {
-    
-    int palinStart = 0;
-    int palinLen = 0;
-    
-    public String longestPalindrome(String s) {
-        
-        if(s.length()<2 || s==null) return s;
-        
-        char[] chars = s.toCharArray();
-        
-        for(int i=0; i<chars.length-1; i++){
-            checkThisPos(chars, i, i);
-            checkThisPos(chars, i, i+1);
-        }
-        return s.substring(palinStart, palinStart+palinLen);
-    }
-    
-    public void checkThisPos(char[] chars, int left, int right){
-        
-        while(left>=0 && right<chars.length && chars[left]==chars[right]){
-            left--;
-            right++;
-        }
-        if(right-left-1>palinLen){
-            palinLen = right-left-1;
-            palinStart = left+1;
-            // System.out.println("palinStart: "+ palinStart+" palinLen: "+palinLen);
-        }
-        
     }
 }
