a58d7647caee7fa0540288672887d55c6270701c
Yuechuan Xiao <xiaoyuechuanz@163.com>
2019-03-03 11:01:27 +0800

    update p53, p70, p746 in java

--- Algorithms/Java/P746-Min-Cost-Climbing-Stairs.java
+++ Algorithms/Java/P746-Min-Cost-Climbing-Stairs.java
@@ -9,6 +9,7 @@ class Solution {
         dp[0] = cost[0];
         dp[1] = cost[1];        
         
+        /* 递推公式 dp[i] = min{dp[i - 1], dp[i -2] + cost[i]} */
         for (int i = 2; i < n; ++i) {
             dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
         }
