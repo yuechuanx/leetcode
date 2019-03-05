9bd3ec640464f0ef5d0c32e585cf2ce635b8327f
Yuechuan Xiao <xiaoyuechuanz@163.com>
2019-03-06 00:01:24 +0800

    update p9 in java

--- Algorithms/Cpp/P9-Palindrome-Number.cpp
+++ Algorithms/Cpp/P9-Palindrome-Number.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-      if(x < 0) return false;
+      if(x < 0) {
+        return false;
+      }
+      
+      /* 取出最高位数 */
       int div = 1;
       while(x / div >= 10) div *= 10;
+      
       while(x > 0) {
         int left = x / div;
         int right = x % 10;
-        if(left != right) return false;
+        if(left != right) {
+          return false;
+        }
         x = (x % div) / 10;
-        div /= 100;
+        div /= 100; // 一次取出两个数字
       }
       return true;
     }
---
+++ Algorithms/Java/P9-palindrome-number.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ *
+ * https://leetcode.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (42.01%)
+ * Total Accepted:    516.6K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '121'
+ *
+ * Determine whether an integer is a palindrome. An integer is a palindrome
+ * when it reads the same backward as forward.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 121
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it
+ * becomes 121-. Therefore it is not a palindrome.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a
+ * palindrome.
+ * 
+ * 
+ * Follow up:
+ * 
+ * Coud you solve it without converting the integer to a string?
+ * 
+ */
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int div = 1;
+        while (x / div >= 10) {
+            div *= 10;
+        }
+
+        while (x > 0) {
+            int hi = x / div;
+            int lo = x % 10;
+            if (hi != lo) {
+                return false;
+            }
+            x = (x % div) / 10;
+            div /= 100;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isPalindrome(121));
+    }
+}
+
