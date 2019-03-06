e7e26dc4d6cd95577c5c97475361c932cf84577a
Yuechuan Xiao <xiaoyuechuanz@163.com>
2019-03-06 10:43:18 +0800

    update p129 in java

---
+++ Algorithms/Java/129.sum-root-to-leaf-numbers.java
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=129 lang=java
+ *
+ * [129] Sum Root to Leaf Numbers
+ *
+ * https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (41.43%)
+ * Total Accepted:    172.7K
+ * Total Submissions: 416.8K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path
+ * could represent a number.
+ * 
+ * An example is the root-to-leaf path 1->2->3 which represents the number
+ * 123.
+ * 
+ * Find the total sum of all root-to-leaf numbers.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * Output: 25
+ * Explanation:
+ * The root-to-leaf path 1->2 represents the number 12.
+ * The root-to-leaf path 1->3 represents the number 13.
+ * Therefore, sum = 12 + 13 = 25.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,9,0,5,1]
+ * ⁠   4
+ * ⁠  / \
+ * ⁠ 9   0
+ * / \
+ * 5   1
+ * Output: 1026
+ * Explanation:
+ * The root-to-leaf path 4->9->5 represents the number 495.
+ * The root-to-leaf path 4->9->1 represents the number 491.
+ * The root-to-leaf path 4->0 represents the number 40.
+ * Therefore, sum = 495 + 491 + 40 = 1026.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        List<Integer> res = new ArrayList();
+        int sum = 0;
+
+        dfs(root, res, 0);
+
+        for (Integer i : res) {
+            sum += i;
+        }
+        return sum;
+    }
+    
+    private void dfs(TreeNode node, List<Integer> list, int n) {
+        if (node == null) 
+            return;
+            
+        n += node.val;
+
+        if (node.left == null && node.right == null) {
+            list.add(n);
+            return;
+        }
+    
+        dfs(node.left, list, n * 10);
+        dfs(node.right, list, n * 10);
+    }
+} 
+
