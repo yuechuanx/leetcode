948f97a0bf41f95feb1cbc036fb4cb76e9cfedfd
Yuechuan Xiao <xiaoyuechuanz@163.com>
2019-03-13 23:31:14 +0800

    update p235, p671 in java

---
+++ Algorithms/Java/P235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=235 lang=java
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (43.49%)
+ * Total Accepted:    260.8K
+ * Total Submissions: 598.3K
+ * Testcase Example:  '[6,2,8,0,4,7,9,null,null,3,5]\n2\n8'
+ *
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of
+ * two given nodes in the BST.
+ * 
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of itself).”
+ * 
+ * Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant
+ * of itself according to the LCA definition.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the BST.
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode node = root;
+
+        while (node != null) {
+            if (p.val > node.val && q.val > node.val) {
+                node = node.right;
+            } else if (p.val < node.val && q.val < node.val) {
+                node = node.left;
+            } else {
+                return node;
+            }
+        }
+
+        return null;
+    }
+}
+
