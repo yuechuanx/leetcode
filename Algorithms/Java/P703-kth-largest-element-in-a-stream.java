d789bc74eb037938247a22ce4dcac7e37d6855f2
Yuechuan <xiaoyuechuanz@163.com>
2019-03-15 23:56:16 +0800

    update p703, rename some files

---
+++ Algorithms/Java/P703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,71 @@
+import java.util.PriorityQueue;
+
+/*
+ * @lc app=leetcode id=703 lang=java
+ *
+ * [703] Kth Largest Element in a Stream
+ *
+ * https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
+ *
+ * algorithms
+ * Easy (45.04%)
+ * Total Accepted:    25K
+ * Total Submissions: 55K
+ * Testcase Example:  '["KthLargest","add","add","add","add","add"]\n[[3,[4,5,8,2]],[3],[5],[10],[9],[4]]'
+ *
+ * Design a class to find the kth largest element in a stream. Note that it is
+ * the kth largest element in the sorted order, not the kth distinct element.
+ * 
+ * Your KthLargest class will have a constructor which accepts an integer k and
+ * an integer array nums, which contains initial elements from the stream. For
+ * each call to the method KthLargest.add, return the element representing the
+ * kth largest element in the stream.
+ * 
+ * Example:
+ * 
+ * 
+ * int k = 3;
+ * int[] arr = [4,5,8,2];
+ * KthLargest kthLargest = new KthLargest(3, arr);
+ * kthLargest.add(3);   // returns 4
+ * kthLargest.add(5);   // returns 5
+ * kthLargest.add(10);  // returns 5
+ * kthLargest.add(9);   // returns 8
+ * kthLargest.add(4);   // returns 8
+ * 
+ * 
+ * Note: 
+ * You may assume that nums' length ≥ k-1 and k ≥ 1.
+ * 
+ */
+class KthLargest {
+
+    final int k;
+    final PriorityQueue<Integer> pq;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue();
+        
+        for (int num : nums)
+            add(num);
+    }
+    
+    public int add(int val) {
+        if (pq.size() < k)
+            pq.offer(val);
+        else if (pq.peek() < val) {
+            pq.poll();
+            pq.offer(val);
+        }
+
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
+
---
+++ Algorithms/Java/P94-binary-tree-inorder-traversal.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=94 lang=java
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (55.14%)
+ * Total Accepted:    413.8K
+ * Total Submissions: 750.5K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,3,2]
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList();
+
+        inorderTraversal(root, res);
+
+        return res;
+    }
+
+    private void inorderTraversal(TreeNode node, List<Integer> res) {
+        if (node == null) return;
+
+        inorderTraversal(node.left, res);
+        res.add(node.val);
+        inorderTraversal(node.right, res);
+    }
+}
+
