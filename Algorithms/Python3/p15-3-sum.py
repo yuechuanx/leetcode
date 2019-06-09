d81ced1548d03274e79309e81fa2c4eb0645a185
Yuechuan <xiaoyuechuanz@163.com>
2019-06-09 18:41:17 +0800

    ignore files and update p15 in python3

---
+++ Algorithms/Python3/p15-3-sum.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+# https://leetcode.com/problems/3sum/description/
+#
+# algorithms
+# Medium (24.06%)
+# Likes:    3830
+# Dislikes: 427
+# Total Accepted:    558.5K
+# Total Submissions: 2.3M
+# Testcase Example:  '[-1,0,1,2,-1,-4]'
+#
+# Given an array nums of n integers, are there elements a, b, c in nums such
+# that a + b + c = 0? Find all unique triplets in the array which gives the sum
+# of zero.
+# 
+# Note:
+# 
+# The solution set must not contain duplicate triplets.
+# 
+# Example:
+# 
+# 
+# Given array nums = [-1, 0, 1, 2, -1, -4],
+# 
+# A solution set is:
+# [
+# ⁠ [-1, 0, 1],
+# ⁠ [-1, -1, 2]
+# ]
+# 
+# 
+#
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums = sorted(nums)
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            left = i + 1
+            right = len(nums) - 1
+            
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    left += 1
+
+        return res
